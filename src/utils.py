c370aa1b2a1156205c77d1ad3dfe5614d15a06ed
npanj <nat.panjar@gmail.com>
2021-12-10 19:31:03 +0700

    utils: delete old adf

--- src/utils.py
+++ src/utils.py
@@ -2,29 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from statsmodels.tsa.stattools import adfuller
 
-
-def adf_test_autolag(timeseries: pd.Series, lag: int = None) -> None:
-    print("Results of Dickey-Fuller Test:")
-
-    if lag is None:
-        dftest = adfuller(timeseries, autolag="AIC")
-    else:
-        dftest = adfuller(timeseries, lag=lag)
-
-    dfoutput = pd.Series(
-        dftest[0:4],
-        index=[
-            "Test Statistic",
-            "p-value",
-            "#Lags Used",
-            "Number of Observations Used",
-        ],
-    )
-    for key, value in dftest[4].items():
-        dfoutput["Critical Value (%s)" % key] = f"{value:.6f}"
-    print(dfoutput)
 
 
 def gen_weekday_num(day: int) -> int:
@@ -40,7 +18,7 @@ def gen_weekday_num(day: int) -> int:
         return 5
     elif day == "Saturday":
         return 6
-    elif day == "Sunday":          
+    elif day == "Sunday":
         return 7
 
 
