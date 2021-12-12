1bfc6c5cbdc6078cb4068f358b9855aebb793f64
npanj <nat.panjar@gmail.com>
2021-12-12 17:50:39 +0700

    update auto arima

--- src/models/timeseries/autoARIMA.py
+++ src/models/timeseries/autoARIMA.py
@@ -42,30 +42,21 @@ class autoARIMA:
         for store_id in tqdm(store_ids):
             data_store = data[data.store_id == store_id]
             item_ids = autoARIMA._get_store_ids(data_store)
+            results = {}
 
             for item_id in tqdm(item_ids, leave=False):
                 data_store_item = data_store_item[data_store_item.item_id == item_id]
-                integrated_order = searchStationarySeriesADF(data_store_item['sales'])
-                
-                model = AutoARIMA(
-                        start_p=1,
-                        start_q=1,
-                        max_p=30,
-                        max_q=30
-                        d=integrated_order,
-                        max_d=integrated_order,
-                        seasonal=False,
-                        start_P=0,
-                        D=0,
-                        trace=True,
-                        error_action='ignore',
-                        suppress_warnings=True,
-                        stepwise=True,
-                        alpha=0.05
-                      )
+                integrated_order = searchStationarySeriesADF(data_store_item["sales"])
+                arima_order = autoARIMA._perform_auto_arima(
+                    ts=data_store_item["sales"], diff_order=integrated_order
+                )
+                results[str(store_id)] = {str(item_id): arima_order}
+        
+        config["ARIMA_orders"] = results
+        save_json(config, asset_dir,  "config.json")
 
     @staticmethod
-    def _get_store_ids(data: pd.DataFrame):
+    def _get_store_ids(data: pd.DataFrame) -> list:
 
         # sort store_ids from largest to smallest revenue
         total_store_revenue = data.groupby("store_id")["revenue"].sum()
@@ -74,9 +65,34 @@ class autoARIMA:
         return store_ids
 
     @staticmethod
-    def _get_item_ids(data: pd.DataFrame):
+    def _get_item_ids(data: pd.DataFrame) -> list:
 
         total_item_revenue = data.groupby("item_id")["revenue"].sum()
         item_ids = total_item_revenue.sort_values(ascending=False).index
 
         return item_ids
+
+    @staticmethod
+    def _perform_auto_arima(ts: pd.Series, diff_order: int) -> tuple:
+
+        model = AutoARIMA(
+            start_p=1,
+            start_q=1,
+            max_p=30,
+            max_q=30,
+            d=diff_order,
+            max_d=diff_order,
+            seasonal=False,
+            start_P=0,
+            D=0,
+            trace=True,
+            error_action="ignore",
+            suppress_warnings=True,
+            stepwise=True,
+            alpha=0.05,
+        )
+
+        model.fit(ts)
+        arima_order = model.get_fitted_params()["order"]
+
+        return arima_order
