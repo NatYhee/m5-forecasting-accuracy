d2b8d7ebed0934529cbb02d7b93b841f7bd243b0
npanj <nat.panjar@gmail.com>
2021-12-26 10:38:08 +0700

    reformat datset maker

--- src/jobs/dataset_maker.py
+++ src/jobs/dataset_maker.py
@@ -5,7 +5,9 @@ import pandas as pd
 from src.utils.import_downcasting import import_downcasting
 
 
-def make_dataset(raw_data_dir:str, export_dir:str, main_file:str, store_id:str = None):
+def make_dataset(
+    raw_data_dir: str, export_dir: str, main_file: str, store_id: str = None
+):
     os.makedirs(export_dir, exist_ok=True)
 
     sales_df = import_downcasting(os.path.join(raw_data_dir, main_file))
@@ -17,16 +19,33 @@ def make_dataset(raw_data_dir:str, export_dir:str, main_file:str, store_id:str =
     merged_df = merge_dataset(sales_df, calendar_df, price_df)
     merged_df.to_csv(os.path.join(export_dir, "dataset.csv"), index=False)
 
-def filter_store(sales_df:pd.DataFrame, store_id:str):
+
+def filter_store(sales_df: pd.DataFrame, store_id: str):
     if store_id == None:
         return sales_df
     else:
         return sales_df[sales_df.store_id == store_id]
 
-def merge_dataset(sales_df:pd.DataFrame, calendar_df:pd.DataFrame, price_df:pd.DataFrame):
-    sales_df = pd.melt(sales_df, id_vars=['id', 'item_id', 'dept_id', 'cat_id', 'store_id', 'state_id'], var_name="date_code", value_name ="sales")
-    sales_df = pd.merge(left=sales_df, right=calendar_df, left_on='date_code', right_on='d', how='left')
-    sales_df = pd.merge(left=sales_df, right=price_df, left_on=['store_id', 'item_id', 'wm_yr_wk'], right_on=['store_id', 'item_id', 'wm_yr_wk'], how='left')
+
+def merge_dataset(
+    sales_df: pd.DataFrame, calendar_df: pd.DataFrame, price_df: pd.DataFrame
+):
+    sales_df = pd.melt(
+        sales_df,
+        id_vars=["id", "item_id", "dept_id", "cat_id", "store_id", "state_id"],
+        var_name="date_code",
+        value_name="sales",
+    )
+    sales_df = pd.merge(
+        left=sales_df, right=calendar_df, left_on="date_code", right_on="d", how="left"
+    )
+    sales_df = pd.merge(
+        left=sales_df,
+        right=price_df,
+        left_on=["store_id", "item_id", "wm_yr_wk"],
+        right_on=["store_id", "item_id", "wm_yr_wk"],
+        how="left",
+    )
     return sales_df
 
 
