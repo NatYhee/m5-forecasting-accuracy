6907b2b6c8ec369f8c533353f1fcb9ab870d6837
npanj <nat.panjar@gmail.com>
2022-02-07 09:15:25 +0700

    adding plotting script

---
+++ src/utils/plotting.py
@@ -0,0 +1,47 @@
+"""Methods for plotting
+"""
+from matplotlib import figure
+from matplotlib.backends.backend_pdf import PdfPages
+
+
+class PdfFile:
+    """PdfPages object to store figures as pdf pages."""
+
+    def __init__(self, fig_path):
+        """Open file.
+
+        Args:
+            fig_path (str): Path to store the pdf file.
+        """
+        self.pdf = PdfPages(fig_path)
+
+    def close(self):
+        """Close file."""
+        self.pdf.close()
+
+    def save_fig(self, data, title):
+        """Visualizes the metric scores
+
+        Args:
+            orders (pd.DataFrame): The orders DataFrame.
+            title (str): To be displayed at the top of the figure.
+        """
+        # fill missing days as nan to break the lines in the plot
+        data.sales = data.sales.asfreq("D")
+        data.prediction = data.prediction.asfreq("D")
+
+        fig = figure.Figure(figsize=(12, 12))
+        axs = fig.subplots(squeeze=False, sharex=True, sharey=True)
+        axs = axs.flat
+
+        axs[0].plot(
+            data.index, data.sales, label="sales", alpha=0.5, c="C0"
+        )
+        axs[0].plot(
+            data.index, data.prediction, label="prediction", alpha=0.5, c="C1"
+        )
+
+        fig.suptitle(title)
+
+        self.pdf.savefig(fig)
+        fig.clf()
