07417c7979c82ae94a14f9c23c524c99198871d0
natpan <39719800+NatYhee@users.noreply.github.com>
2022-03-03 17:31:58 +0700

    Feat/analysis arima residual (#2)
    * adding seaborn to pipfile.lock
    
    * adding seaborn to pipfile.lock
    
    * adding plotting script
    
    * update the script
    
    * revise figsize plotting
    
    * update experiment notebook
    
    * autoARIMA: adding method to calculate wmape
    
    * plotting: reformatting code
    
    * autoARIMA: fixing bug
    
    * autoARIMA: update the script
    
    * plotting: adding legend
    
    * adding notebook analysis residual
    
    * adding utils function for analysis auto_arima_residual
    
    * update analysis on residual
    
    * update notebook
    
    * update notebook
    
    * update utils
    
    * reformatting notebook
    
    * update utils
    
    * update utils
    
    * update notebook
    
    * update notebook

---
+++ src/utils/plotting.py
@@ -0,0 +1,43 @@
+"""Methods for plotting
+"""
+from matplotlib import figure
+from matplotlib.backends.backend_pdf import PdfPages
+
+
+class PdfFile:
+    """PdfPages object to store figures as pdf pages."""
+
+    def __init__(self, fig_path):
+        """Open file.
+
+        Args:
+            fig_path (str): Path to store the pdf file.
+        """
+        self.pdf = PdfPages(fig_path)
+
+    def close(self):
+        """Close file."""
+        self.pdf.close()
+
+    def save_fig(self, data, title):
+        """Visualizes the metric scores
+
+        Args:
+            data (pd.DataFrame): The Dataframe contains sales and forecasts data
+        """
+        # fill missing days as nan to break the lines in the plot
+        data.sales = data.sales.asfreq("D")
+        data.prediction = data.prediction.asfreq("D")
+
+        fig = figure.Figure(figsize=(8, 8))
+        axs = fig.subplots(squeeze=False, sharex=True, sharey=True)
+        axs = axs.flat
+
+        axs[0].plot(data.index, data.sales, label="sales", alpha=0.5, c="C0")
+        axs[0].plot(data.index, data.prediction, label="prediction", alpha=0.5, c="C1")
+        axs[0].legend(frameon=False)
+
+        fig.suptitle(title)
+
+        self.pdf.savefig(fig)
+        fig.clf()
